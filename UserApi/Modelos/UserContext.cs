ef998cb696a121f32d42510c6ea7d91d3e89bf56
frank-cka <fchongkan@gmail.com>
2023-11-06 10:09:49 -0600

    userApi commit

---
+++ UserApi/Modelos/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace UserApi.Modelos;
+
+public class UserContext : DbContext
+{
+    public UserContext(DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<UserItem> UserItems { get; set; } = null!;
+}
---
+++ UserApi/Modelos/UserItem.cs
@@ -0,0 +1,13 @@
+﻿namespace UserApi.Modelos
+{
+    public class UserItem
+    {
+        public  int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string ap1 { get; set; }
+
+        public string email { get; set; }
+    }
+}
